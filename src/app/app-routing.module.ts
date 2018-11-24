be5d0e25c4ab94b9ddcb7e7be2856a2efeac8297
Eric Chang <efrochang@gmail.com>
2018-11-24 11:21:35 -0600

    [KEY] Redirect root to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
 
 const routes: Array<Route> = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'settings', component: SettingsComponent }
 ];

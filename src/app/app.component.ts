ce31e3eeaea358255bb7e6e6b48eab4cff7ff83c
Eric Chang <efrochang@gmail.com>
2018-11-24 12:34:31 -0600

    Create links to navigate Routes 2.  Using ngFor and [routerLink]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  routes: Array<any> = [
+    { routeName: 'Home', routePath: 'home'},
+    { routeName: 'Settings', routePath: 'settings'}
+  ]
 }

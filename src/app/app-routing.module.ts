e2bdb156d23adf7597e92a0fee82b14f2e352f90
Eric Chang <efrochang@gmail.com>
2018-11-23 18:10:49 -0600

    Define my routes here!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Array<Route> = [
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

12fc14038e2f14fd5274a5bdae86af75cbd1878c
Eric Chang <efrochang@gmail.com>
2018-11-23 18:51:06 -0600

    [KEY] Route Configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Array<Route> = [
-  
+  { path: 'home', component: HomeComponent },
+  { path: 'settings', component: SettingsComponent }
 ];
 
 @NgModule({

3736483207f2f00b41f03cd5ebcc6c609f8b9c38
Eric Chang <efrochang@gmail.com>
2018-11-24 11:31:43 -0600

    Page Not Found route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Array<Route> = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'settings', component: SettingsComponent }
+  { path: 'settings', component: SettingsComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
